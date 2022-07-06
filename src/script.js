ba7a2597de76ee1db0d39d22c537b918ec40a6f8
Antonio Manzari <antoniomanzari@gmail.com>
2022-07-05 22:48:02 -0400

    init commit, half done with tut. scroll FX works

---
+++ src/script.js
@@ -0,0 +1,176 @@
+import './style.css'
+import * as THREE from 'three'
+import * as dat from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
+const parameters = {
+    materialColor: '#ffeded'
+}
+
+gui
+    .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+    })
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * 
+ * Objects
+ */
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
+
+const material = new THREE.MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture 
+})
+
+const objectsDistance = 4;
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1,0.4,16,60),
+    material
+)
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1,2,32),
+    material
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8,0.35,100,16),
+    material
+)
+
+mesh1.position.y = objectsDistance *  0
+mesh2.position.y = objectsDistance * -1
+mesh3.position.y = objectsDistance * -2
+
+mesh1.position.x = 2
+mesh2.position.x = -2
+mesh3.position.x = 2
+
+scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [mesh1, mesh2, mesh3 ]
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+/**
+ * Camera
+ */
+// Camera parent
+const camParent = new THREE.Group()
+scene.add(camParent)
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+camParent.add(camera)
+
+/**
+ * 
+ * Lights
+ */
+
+const light = new THREE.DirectionalLight('#FFFFFF',1)
+light.position.set(1,1,0)
+scene.add(light)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY
+
+ window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+})
+
+/**
+ * Cursor
+ */
+
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - 0.5
+    cursor.y = e.clientY / sizes.height - 0.5
+})
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    
+    // Animate Cam
+    camera.position.y = (-scrollY / sizes.height) * objectsDistance
+
+    const parallaxX = cursor.x
+    const parallaxY = - cursor.y
+    camParent.position.x += (parallaxX - camParent.position.x) * 0.02
+    camParent.position.y += (parallaxY - camParent.position.y) * 0.02
+
+    // Animate
+    for(const mesh of sectionMeshes) {
+        mesh.rotation.x = elapsedTime * 0.1
+        mesh.rotation.y = elapsedTime * 0.12
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
